c97335f954782ba9090545ed9b4fc74c6c85f97d
ravil-ishkinin <i.r.v.95@mail.ru>
2026-04-16 10:38:32 +0000

    Применяем Pydantic в HTTP API клиентах

--- api_client_get_documents.py
+++ api_client_get_documents.py
@@ -11,7 +11,7 @@ create_user_response = users_gateway_client.create_user()
 print('Create user data:', create_user_response)
 
 # # Открыть кредитный счет
-accounts_gateway_client = accounts_gateway_client.open_debit_card_account(user_id=create_user_response['user']['id'])
+accounts_gateway_client = accounts_gateway_client.open_debit_card_account(user_id=create_user_response.user.id)
 print('Open credit card:', accounts_gateway_client)
 
 # Получить документ тарифа
