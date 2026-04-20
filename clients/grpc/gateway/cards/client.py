9e21ea80b1e98d160524b77e1ce50e5e46651849
ravil-ishkinin <i.r.v.95@mail.ru>
2026-04-20 21:17:38 +0000

    Знакомство с gRPC API клиентами. Практика: Написание gRPC API клиента

---
+++ clients/grpc/gateway/cards/client.py
@@ -0,0 +1,68 @@
+from grpc import Channel
+from tools.fakers import fake
+from clients.grpc.client import GRPCClient
+from clients.grpc.gateway.client import build_gateway_grpc_client
+from contracts.services.gateway.cards.cards_gateway_service_pb2_grpc import CardsGatewayServiceStub
+from contracts.services.gateway.cards.rpc_issue_physical_card_pb2 import (
+    IssuePhysicalCardResponse,
+    IssuePhysicalCardRequest
+)
+from contracts.services.gateway.cards.rpc_issue_virtual_card_pb2 import (
+    IssueVirtualCardRequest,
+    IssueVirtualCardResponse
+)
+
+class CardsGatewayGRPCClient(GRPCClient):
+    """
+    gRPC-клиент для взаимодействия с CardGatewayService.
+    Предоставляет высокоуровневые методы для получения и создания пользователей.
+    """
+
+    def __init__(self, channel: Channel):
+        """
+        Инициализация клиента с указанным gRPC-каналом.
+
+        :param channel: gRPC-канал для подключения к UsersGatewayService.
+        """
+        super().__init__(channel)
+        self.stub = CardsGatewayServiceStub(channel)
+
+    def issue_virtual_card_api(self, request: IssueVirtualCardRequest) -> IssueVirtualCardResponse:
+        """
+        Выпуск физической карты.
+        """
+        return self.stub.IssueVirtualCard(request)
+
+    def issue_physical_card_api(self, request: IssuePhysicalCardRequest) -> IssuePhysicalCardResponse:
+        """
+        Выпуск виртуальной карты.
+        """
+        return self.stub.IssuePhysicalCard(request)
+
+    def issue_virtual_card(self, user_id: str, account_id: str) -> IssueVirtualCardResponse:
+        """
+        Новый метод выпуска виртуальной карты.
+        """
+        request = IssueVirtualCardRequest(
+            user_id=user_id,
+            account_id=account_id
+        )
+        return self.issue_virtual_card_api(request)
+
+    def issue_physical_card(self, user_id: str, account_id: str) -> IssuePhysicalCardResponse:
+        """
+        Новый метод выпуска физической карты.
+        """
+        request = IssuePhysicalCardRequest(
+            user_id=user_id,
+            account_id=account_id
+        )
+        return self.issue_physical_card_api(request)
+
+def build_cards_gateway_grpc_client() -> CardsGatewayGRPCClient:
+    """
+    Фабрика для создания экземпляра CardsGatewayGRPCClient.
+
+    :return: Инициализированный клиент для CardsGatewayService.
+    """
+    return CardsGatewayGRPCClient(channel=build_gateway_grpc_client())
