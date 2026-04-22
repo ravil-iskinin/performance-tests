3d5078317493f110d0ed5eebeb4c4a8f7a372596
ravil-ishkinin <i.r.v.95@mail.ru>
2026-04-22 12:52:07 +0000

    fix: rename variable

--- grpc_api_client_get_documents.py
+++ grpc_api_client_get_documents.py
@@ -11,13 +11,13 @@ create_user_response = users_gateway_client.create_user()
 print('Create user data:', create_user_response)
 
 # # Открыть кредитный счет
-accounts_gateway_client = accounts_gateway_client.open_debit_card_account(user_id=create_user_response.user.id)
-print('Open credit card:', accounts_gateway_client)
+open_debit_card_account_response = accounts_gateway_client.open_debit_card_account(user_id=create_user_response.user.id)
+print('Open credit card:', open_debit_card_account_response)
 
 # Получить документ тарифа
-document_tariff = documents_gateway_client.get_tariff_document(account_id=accounts_gateway_client.account.id)
-print('Get tariff document:', document_tariff)
+document_tariff_response = documents_gateway_client.get_tariff_document(account_id=open_debit_card_account_response.account.id)
+print('Get tariff document:', document_tariff_response)
 
 # Получить документ контракта
-document_contract = documents_gateway_client.get_contract_document(account_id=accounts_gateway_client.account.id)
-print('Get contract document:', document_contract)
+document_contract_response = documents_gateway_client.get_contract_document(account_id=open_debit_card_account_response.account.id)
+print('Get contract document:', document_contract_response)
