aeacfa64cca61e3f6b902b1e92af91a083ab7249
ravil-ishkinin <i.r.v.95@mail.ru>
2026-05-20 13:37:00 +0000

    Практика нагрузочного тестирования. Часть 3. Практика: Выпуск виртуальной карты существующим пользователем

---
+++ scenarios/grpc/gateway/existing_user_issue_virtual_card/scenario.py
@@ -0,0 +1,36 @@
+from locust import events, task
+from locust.env import Environment
+from clients.grpc.gateway.locust import GatewayGRPCTaskSet
+from seeds.scenarios.existing_user_issue_virtual_card import ExistingUserIssueVirtualCardSeedsScenario
+from seeds.schema.result import SeedUserResult
+from tools.locust.user import LocustBaseUser
+
+@events.init.add_listener
+def init(environment: Environment, **kwargs):
+    seeds_scenario = ExistingUserIssueVirtualCardSeedsScenario()
+
+    seeds_scenario.build()
+    environment.seeds = seeds_scenario.load()
+
+class ExistingUserIssueVirtualCardTaskSet(GatewayGRPCTaskSet):
+
+    seed_user: SeedUserResult
+
+    def on_start(self) -> None:
+        super().on_start()
+        self.seed_user = self.user.environment.seeds.get_random_user()
+
+    @task(2)
+    def get_accounts(self):
+        # Получаем список счетов пользователя
+        self.accounts_gateway_client.get_accounts(user_id=self.seed_user.user_id)
+
+    @task(1)
+    def issue_virtual_c(self):
+        self.cards_gateway_client.issue_virtual_card(
+            user_id=self.seed_user.user_id,
+            account_id=self.seed_user.debit_card_accounts[0].account_id
+        )
+
+class ExistingUserIssueVirtualUserScenario(LocustBaseUser):
+    tasks = [ExistingUserIssueVirtualCardTaskSet]
